1ca1e1d8b1353d88a51f70d39e3d99bec5cf3bd8
Stuart Nielsen <stuart.nielsen@wavetronix.com>
2022-08-01 11:24:34 -0600

    Make stats easier to read

--- src/Components/CenterStatCard.jsx
+++ src/Components/CenterStatCard.jsx
@@ -5,18 +5,18 @@ export default function CenterStatCard() {
     <>
       <Card style={{ margin: '10px', padding: '10px', textAlign: 'center', width: '255x', height: '520px' }}>
         <h3>Stats</h3>
-        <h5>Total Rounds / Total Points</h5>
-        <h5>PPR Avg.</h5>
-        <h5>4 Baggers</h5>
-        <h5>Bags Thrown</h5>
-        <h5>Slide / pct.</h5>
-        <h5>Airmail / pct.</h5>
-        <h5>Roll / pct.</h5>
-        <h5>Block / pct.</h5>
-        <h5>Push / pct.</h5>
-        <h5>Woody / pct.</h5>
-        <h5>Bully / pct.</h5>
-        <h5>Foul / pct.</h5>
+        <h5 style={{ backgroundColor: 'lightGrey', marginBottom: '-10px' }}>Total Rounds / Total Points</h5>
+        <h5 style={{ marginBottom: '-10px' }}>PPR Avg.</h5>
+        <h5 style={{ backgroundColor: 'lightGrey', marginBottom: '-10px' }}>4 Baggers</h5>
+        <h5 style={{ marginBottom: '-10px' }}>Bags Thrown</h5>
+        <h5 style={{ backgroundColor: 'lightGrey', marginBottom: '-10px' }}>Slide / pct.</h5>
+        <h5 style={{ marginBottom: '-10px' }}>Airmail / pct.</h5>
+        <h5 style={{ backgroundColor: 'lightGrey', marginBottom: '-10px' }}>Roll / pct.</h5>
+        <h5 style={{ marginBottom: '-10px' }}>Block / pct.</h5>
+        <h5 style={{ backgroundColor: 'lightGrey', marginBottom: '-10px' }}>Push / pct.</h5>
+        <h5 style={{ marginBottom: '-10px' }}>Woody / pct.</h5>
+        <h5 style={{ backgroundColor: 'lightGrey', marginBottom: '-10px' }}>Bully / pct.</h5>
+        <h5 style={{ marginBottom: '-10px' }}>Foul / pct.</h5>
       </Card>
     </>
   )
--- src/Components/PlayerStats.jsx
+++ src/Components/PlayerStats.jsx
@@ -5,34 +5,34 @@ export default function PlayerStats({ player, totalRounds }) {
     <>
       <Card style={{ margin: '10px', padding: '10px', textAlign: 'center', width: '255x', height: '520px' }}>
         <h3>{player.teamName}</h3>
-        <h5>
+        <h5 style={{ backgroundColor: 'lightGrey', marginBottom: '-10px' }}>
           {totalRounds} / {player.totalPoints}
         </h5>
-        <h5>{player.pprAvg.toFixed(2)}</h5>
-        <h5>{player.fourBaggers}</h5>
-        <h5>{player.bagsThrown}</h5>
-        <h5>
+        <h5 style={{ marginBottom: '-10px' }}>{player.pprAvg.toFixed(2)}</h5>
+        <h5 style={{ backgroundColor: 'lightGrey', marginBottom: '-10px' }}>{player.fourBaggers}</h5>
+        <h5 style={{ marginBottom: '-10px' }}>{player.bagsThrown}</h5>
+        <h5 style={{ backgroundColor: 'lightGrey', marginBottom: '-10px' }}>
           {player.slide} / {((player.slide / player.bagsThrown) * 100).toFixed(2)}
         </h5>
-        <h5>
+        <h5 style={{ marginBottom: '-10px' }}>
           {player.airmail} / {((player.airmail / player.bagsThrown) * 100).toFixed(2)}
         </h5>
-        <h5>
+        <h5 style={{ backgroundColor: 'lightGrey', marginBottom: '-10px' }}>
           {player.roll} / {((player.roll / player.bagsThrown) * 100).toFixed(2)}
         </h5>
-        <h5>
+        <h5 style={{ marginBottom: '-10px' }}>
           {player.block} / {((player.block / player.bagsThrown) * 100).toFixed(2)}
         </h5>
-        <h5>
+        <h5 style={{ backgroundColor: 'lightGrey', marginBottom: '-10px' }}>
           {player.push} / {((player.push / player.bagsThrown) * 100).toFixed(2)}
         </h5>
-        <h5>
+        <h5 style={{ marginBottom: '-10px' }}>
           {player.woody} / {((player.woody / player.bagsThrown) * 100).toFixed(2)}
         </h5>
-        <h5>
+        <h5 style={{ backgroundColor: 'lightGrey', marginBottom: '-10px' }}>
           {player.bully} / {((player.bully / player.bagsThrown) * 100).toFixed(2)}
         </h5>
-        <h5>
+        <h5 style={{ marginBottom: '-10px' }}>
           {player.foul}/ {((player.foul / player.bagsThrown) * 100).toFixed(2)}
         </h5>
       </Card>
